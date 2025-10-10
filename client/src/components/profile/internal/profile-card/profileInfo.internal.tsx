b2402788aa0fbb77393965e8ff40e5306a8b488c
jebesh67 <jebesh67@gmail.com>
2025-10-10 21:28:49 +0530

    css update to profileInfo.internal.tsx

--- client/src/components/profile/internal/profile-card/profileInfo.internal.tsx
+++ client/src/components/profile/internal/profile-card/profileInfo.internal.tsx
@@ -10,18 +10,18 @@ export const ProfileInfoInternal = ({currentUser}: Props) => {
   return (
     <div className={ "grid grid-cols-10 max-w-200 px-6" }>
       <div className={ "col-span-8 flex flex-col justify-center" }>
-        <h3 className={ "font-semibold" }>{ currentUser.name }</h3>
+        <h3 className={ "font-semibold css-text-sub-head" }>{ currentUser.name }</h3>
         <div className={ "flex gap-13 sm:gap-20 md:gap-26 xl:gap-30 pt-1" }>
           <div>
-            <h2 className={ "font-semibold" }>{ currentUser.counts.postsCount }</h2>
+            <h2 className={ "font-semibold css-text-sub-head" }>{ currentUser.counts.postsCount }</h2>
             <p className={ "leading-3" }>posts</p>
           </div>
           <div>
-            <h2 className={ "font-semibold" }>{ currentUser.counts.followersCount }</h2>
+            <h2 className={ "font-semibold css-text-sub-head" }>{ currentUser.counts.followersCount }</h2>
             <p className={ "leading-3" }>followers</p>
           </div>
           <div>
-            <h2 className={ "font-semibold" }>{ currentUser.counts.postsCount }</h2>
+            <h2 className={ "font-semibold css-text-sub-head" }>{ currentUser.counts.postsCount }</h2>
             <p className={ "leading-3" }>following</p>
           </div>
         </div>
