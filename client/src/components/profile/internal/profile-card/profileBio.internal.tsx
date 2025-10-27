f18979441aa562ef31da082b07fd183ed27d6b79
jebesh67 <jebesh67@gmail.com>
2025-10-27 10:53:36 +0530

    auth page and profile bio UI updates

--- client/src/components/profile/internal/profile-card/profileBio.internal.tsx
+++ client/src/components/profile/internal/profile-card/profileBio.internal.tsx
@@ -9,14 +9,12 @@ export const ProfileBioInternal = ({currentUser}: Props) => {
     <div className={ "col-span-10 space-y-1" }>
       <p className={ "font-semibold mt-4 text-sm sm:hidden" }>{ currentUser.name }</p>
       
-      
       {
         currentUser.bio && (
           <p className={ "font-light text-sm sm:mt-2" }>{ currentUser.bio }
           </p>
         )
       }
-    
     
     </div>
   
