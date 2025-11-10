4dce6888b0c0a3ad929f554bb00cb87d73d97e75
jebesh67 <jebesh67@gmail.com>
2025-11-10 19:59:05 +0530

    add middleware.ts

---
+++ client/src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getAuthToken } from "@/common/utils/cookie/cookie.helper";
+import { PUBLIC_PATHS } from "@/middleware/data/publicPaths.data";
+import { IVerifyAccessBackendResponse } from "@/types/user/response/backend/verifyAccessBackend.response";
+import { request } from "graphql-request";
+import { GRAPHQL_URL } from "@/lib/env/url.variable";
+import VerifyAccessQuery from "@/graphql/user/query/verifyAccess.query.graphql";
+
+export const middleware = async (req: NextRequest): Promise<NextResponse> => {
+  const {pathname} = req.nextUrl;
+  
+  if (PUBLIC_PATHS.some((path: string): boolean => pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+  
+  const token: string = await getAuthToken();
+  
+  if (token) {
+    const response: IVerifyAccessBackendResponse = await request(GRAPHQL_URL, VerifyAccessQuery, {}, {
+      Authorization: `Bearer ${ token }`,
+    });
+    
+    if (!response.verifyAccess.data.accessGranted)
+      return NextResponse.redirect(new URL("/auth", req.url));
+    
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/auth", req.url));
+  }
+};
+
+export const config = {
+  matcher: ["/((?!_next/static|favicon.ico).*)"],
+};
---
+++ client/src/middleware/data/publicPaths.data.ts
@@ -0,0 +1,5 @@
+export const PUBLIC_PATHS: string[] = [
+  "/auth",
+  "/_next",
+  "/favicon.ico",
+];
