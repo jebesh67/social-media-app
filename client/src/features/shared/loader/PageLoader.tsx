20d8c8dfc8947ac7049162f4ba28c8b95f39d282
jebesh67 <jebesh67@gmail.com>
2025-11-29 10:20:58 +0530

    update PageLoader.tsx

--- client/src/features/shared/loader/PageLoader.tsx
+++ client/src/features/shared/loader/PageLoader.tsx
@@ -1,14 +1,22 @@
 import ShinyText from "@/features/shared/effects/shinyText/ShinyText";
+import { useTheme } from "@/core/hooks/theme/useTheme";
+import { clsx } from "clsx";
+import { ifTheme } from "@/core/utils/theme/util/theme.util";
 
 export const PageLoader = () => {
+  const [theme] = useTheme();
+  
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-b from-zinc-800 z-60 to-zinc-900 select-none">
+    <div className={ clsx(
+      "fixed inset-0 flex items-center justify-center bg-gradient-to-b z-60 select-none",
+      ifTheme(theme, "from-zinc-800 to-zinc-900", "from-zinc-100 to-zinc-200"),
+    ) }>
       <ShinyText
         text="SocialMedia"
         disabled={ false }
         speed={ 3 }
         className="text-3xl font-bold"
-        theme={ "dark" }
+        theme={ theme }
       />
     </div>
   );
