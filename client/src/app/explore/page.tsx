f091963ff88e9748d8448148c3c73fa18534ad02
jebesh67 <jebesh67@gmail.con>
2026-01-07 17:12:57 +0530

    add explore page

---
+++ client/src/app/explore/page.tsx
@@ -0,0 +1,11 @@
+import Explore from "@/features/explore/main";
+
+const ExplorePage = () => {
+  return (
+    <>
+      <Explore />
+    </>
+  )
+}
+
+export default ExplorePage;
---
+++ client/src/features/explore/main/index.tsx
@@ -0,0 +1,9 @@
+const Explore = () => {
+  return (
+    <div className={"-mt-18 bg-red-600"}>
+      explore page
+    </div>
+  )
+}
+
+export default Explore;
